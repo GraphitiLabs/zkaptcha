e0aeb5066c769fc36bef2aa816c7b4fd3e706d3a
Andrew Magid <magid.andrew@gmail.com>
2023-03-20 14:35:22 -0400

    merkle + preimage check

---
+++ circuits/contract/README.md
@@ -0,0 +1,5 @@
+# What is this
+
+I ran `nargo codegen-verifier` on two different noir scripts to
+figure out what is boilerplate and what is specific to the different constraints
+/ input variables in noir.
--- circuits/test_hash.py
+++ circuits/test_hash.py
@@ -1,4 +1,5 @@
 import hashlib
+import sys
 
 
 def sha256(byte_array):
@@ -14,18 +15,35 @@ def sha256(byte_array):
     # Return the hash value as a string
     return hex_digest
 
+def pederson(byte_array):
+    # todo: implement pederson hash for testing
+    ...
 
-# UNSAFE. DO NOT USE IN PROD.
+PROGRAM_NAME = "test_hash.py"
+
 if __name__ == "__main__":
-    print("PROGRAM IS UNSAFE.")
-    a = input("hex bytearray (ex: 6fa3): ")
-    new_string = r"\x"
-    for i in range(0, len(a), 2):
-        new_string += a[i:i+2] + r"\x"
+    # note: doesn't work with empty inputs.
+    if len(sys.argv) == 1:
+        a = input("hex bytearray (ex: 6fa3): ")
+    elif len(sys.argv) == 2:
+        a = sys.argv[1]
+        # todo: check if it is string for ease of use
+        # if "\"" in a:
+        #     a = a[1:-1]
+        #     print(sha256([elem.encode("hex") for elem in a]))
+        #     sys.exit(0)
+    else:
+        print(f"usage: python3 {PROGRAM_NAME} or python3 {PROGRAM_NAME} <hex>")
 
-    # Remove the trailing "-" character from the new string
-    new_string = new_string[:-2]
-    new_string = "b\"" + new_string + "\""
-    h = sha256(eval(new_string))
+    if len(a) % 2 != 0:
+        print("invalid hex input")
+        exit(1)
+
+    # parse if real hex value
+    hx = int(a, 16)
+    hx_hex = hex(hx)
+    # trick. converts to string which is prepended with "0x". Need to remove the
+    # first two chars.
+    h = sha256(bytearray.fromhex(hx_hex[2:]))
 
     print(h)
