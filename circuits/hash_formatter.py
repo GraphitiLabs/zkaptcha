f5ee5a5cf37acaa986ee0bc79252d2c29571e0fc
Andrew Magid <magid.andrew@gmail.com>
2023-03-24 13:18:25 -0400

    merkle trees + hashing

--- circuits/hash_formatter.py
+++ circuits/hash_formatter.py
@@ -1,11 +1,14 @@
 import json
 import sys
 
-for line in sys.stdin:
-	hstr = line.strip()
-	print(hstr)
-	if 'Exit' == line.rstrip():
-		break
+if len(sys.argv) > 1:
+	hstr = sys.argv[1]
+else:
+	for line in sys.stdin:
+		hstr = line.strip()
+		print(hstr)
+		if 'Exit' == line.rstrip():
+			break
 
 ret = ['0x' + hstr[i] + hstr[i+1] for i in range(0, len(hstr), 2)]
 sys.stdout.write(json.dumps(ret))
---
+++ ts/merkle.ts
@@ -0,0 +1,27 @@
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import fs from "fs";
+
+const hasha = "0xca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb";
+const hashb = "0x3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d";
+
+function fileToValues(path: string): string[][] {
+	const fileData = fs.readFileSync(path, 'utf-8');
+	const lines = fileData.split('\n');
+	const nonEmptyLines = lines.filter((line) => line.trim() !== '');
+	const lineArrays = nonEmptyLines.map((line) => [line]);
+	return lineArrays;
+}
+
+// (1)
+const values = fileToValues("test_inputs.txt");
+console.log(values);
+
+// (2)
+const tree = StandardMerkleTree.of(values, ["bytes32"]);
+
+// (3)
+console.log('Merkle Root:', tree.root);
+console.log(tree.getProof(["0xca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"]));
+
+// (4)
+// fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
