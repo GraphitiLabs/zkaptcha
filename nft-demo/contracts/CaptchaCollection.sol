d0485bd77a2dbaa4b9a55d46e91513712aa7c854
Ketan Jog <kj2473@columbia.edu>
2023-03-25 19:12:10 -0400

    nft collection created for demo

--- nft-demo/contracts/CaptchaCollection.sol
+++ nft-demo/contracts/CaptchaCollection.sol
@@ -5,16 +5,34 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract CaptchaCollection is ERC721URIStorage {
     uint256 public tokenCounter;
+    uint256 public constant NFT_PRICE = 0.0000 ether;
+    bool public saleIsActive = false;
 
     constructor() public ERC721("Captcha Collection", "CAPT") {
         tokenCounter = 0;
     }
 
+    // modifier onlyOwner() {
+    //     require(msg.sender == owner(), "Only owner can call this function.");
+    //     _;
+    // }
+
     function createToken(string memory tokenURI) public returns (bytes32) {
         require(tokenCounter < 10, "Max number of tokens reached");
+        // require(NFT_PRICE <= msg.value, "Ether value sent is not correct");
+        require(saleIsActive, "Sale must be active to mint NFTs");
         uint256 tokenId = tokenCounter;
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, tokenURI);
         tokenCounter++;
     }
+
+    // function flipSaleState() external onlyOwner {
+    //     saleIsActive = !saleIsActive;
+    // }
+
+    // function withdraw() external onlyOwner {
+    //     uint256 balance = address(this).balance;
+    //     payable(owner()).transfer(balance);
+    // }
 }
--- nft-demo/scripts/save_metadata.py
+++ nft-demo/scripts/save_metadata.py
@@ -4,6 +4,10 @@ import json
 
 metadata_template = {"name": "", "description": "", "image": ""}
 
+BASEPATH = (
+    "https://gateway.pinata.cloud/ipfs/QmSuhmk687AsjVrzNyutA41ysbUnJDfLQ849BykmnB5dDT/"
+)
+
 names = [
     "buterin",
     "saylor",
@@ -18,10 +22,6 @@ names = [
 ]
 
 
-def main():
-    write_metadata(10)
-
-
 def write_metadata(num_tokens):
     # We'll use this array to store the hashes of the metadata
     meta_data_hashes = []
@@ -33,44 +33,40 @@ def write_metadata(num_tokens):
 
         # Name of the collectible set to its token id
         collectible_metadata["name"] = str(names[token_id])
+
         # Description of the collectible set to be "Wata"
         collectible_metadata[
             "description"
         ] = "Captcha art for the names of a few promininent people in the Crypto space"
-        # Path of the artwork to be uploaded to IPFS
-        img_path = f"images/{names[token_id]}.png"
-        with open(img_path, "rb") as f:
-            img_binary = f.read()
-        # Upload the image to IPFS and get the storage address
-        image = upload_to_ipfs(img_binary)
-        # Add the image URI to the metadata
-        image_path = f"<https://ipfs.io/ipfs/{image}>"
+
+        # Image of the collectible set to be the image of the token id
+        image_path = f"{BASEPATH}{names[token_id]}.png"
         collectible_metadata["image"] = image_path
+
         with open(meta_data_filename, "w") as f:
             # Write the metadata locally
             json.dump(collectible_metadata, f)
-        # Upload our metadata to IPFS
-        meta_data_hash = upload_to_ipfs(collectible_metadata)
-        meta_data_path = f"<https://ipfs.io/ipfs/{meta_data_hash}>"
-        # Add the metadata URI to the array
-        meta_data_hashes.append(meta_data_path)
-    with open("metadata/data.json", "w") as f:
-        # Finally, we'll write the array of metadata URIs to a file
-        json.dump(meta_data_hashes, f)
-    return meta_data_hashes
 
 
-def upload_to_ipfs(data):
-    # Get our Pinata credentials from our .env file
-    pinata_api_key = os.environ["PINATA_API_KEY"]
-    pinata_api_secret = os.environ["PINATA_API_SECRET"]
-    endpoint = "<https://api.pinata.cloud/pinning/pinFileToIPFS>"
-    headers = {
-        "pinata_api_key": pinata_api_key,
-        "pinata_secret_api_key": pinata_api_secret,
-    }
-    body = {"file": data}
-    # Make the pin request to Pinata
-    response = requests.post(endpoint, headers=headers, files=body)
-    # Return the IPFS hash where the data is stored
-    return response.json()["IpfsHash"]
+# def upload_to_ipfs(data):
+#     # Get our Pinata credentials from our .env file
+#     pinata_api_key = os.environ["PINATA_API_KEY"]
+#     pinata_api_secret = os.environ["PINATA_API_SECRET"]
+#     endpoint = "<https://api.pinata.cloud/pinning/pinFileToIPFS>"
+#     headers = {
+#         "pinata_api_key": pinata_api_key,
+#         "pinata_secret_api_key": pinata_api_secret,
+#     }
+#     body = {"file": data}
+#     # Make the pin request to Pinata
+#     response = requests.post(endpoint, headers=headers, files=body)
+#     # Return the IPFS hash where the data is stored
+#     return response.json()["IpfsHash"]
+
+
+def main():
+    write_metadata(10)
+
+
+if __name__ == "__main__":
+    main()
