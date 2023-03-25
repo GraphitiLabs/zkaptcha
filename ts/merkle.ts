5c08f3c59b22eb4c66557616db29ebfc51c4ecca
Andrew Magid <magid.andrew@gmail.com>
2023-03-24 21:53:19 -0400

    noir prover output hashes combined

--- ts/merkle.ts
+++ ts/merkle.ts
@@ -22,6 +22,9 @@ const tree = StandardMerkleTree.of(values, ["bytes32"]);
 // (3)
 console.log('Merkle Root:', tree.root);
 console.log(tree.getProof(["0xca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"]));
+console.log(tree.getProof(["0x3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d"]));
+
+
 
 // (4)
 // fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
