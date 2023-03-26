b13dd2dcdfa75fe3cd7b2925b6d2dc6173f24f1d
Ketan Jog <kj2473@columbia.edu>
2023-03-26 02:20:03 -0400

    nft demo mint script

--- nft-demo/scripts/mint.py
+++ nft-demo/scripts/mint.py
@@ -11,14 +11,14 @@ def main():
     # Get our account info
     dev = accounts.load("dev-account")
     # Get the most recent deployment of our contract
-    water_collection = CaptchaCollection[-1]
+    captcha_collection = CaptchaCollection[-1]
     # Check the number of currently minted tokens
-    existing_tokens = water_collection.tokenCounter()
+    existing_tokens = captcha_collection.tokenCounter()
     print("Existing tokens: ", existing_tokens)
 
     # Get the metadata hash for this token's URI
     meta_data_hash = "https://gateway.pinata.cloud/ipfs/QmYseRJwUGHJbqYvTqivquZxE8pmjbTUMoHd6B6S5t4MoA/armstrong.json"
     # Call our createCollectible function to mint a token
-    transaction = water_collection.createToken(meta_data_hash, {"from": dev})
+    transaction = captcha_collection.createToken(meta_data_hash, {"from": dev})
     # Wait for 3 blocks to be created atop our transactions
     transaction.wait(3)
